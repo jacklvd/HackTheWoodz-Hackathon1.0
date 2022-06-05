8b732a0480343fad5c85a601f0eb77477d3e984f
jacklvd <vodanglong3103@gmail.com>
2022-06-04 23:37:16 -0400

    loginpage update

--- backend/projectmanager/urls.py
+++ backend/projectmanager/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.getData, name="get"),
     path('add/', views.addData, name="add"),
+    path('login/', views.loginPage, name='login')
 ]
--- backend/projectmanager/views.py
+++ backend/projectmanager/views.py
@@ -1,14 +1,17 @@
 #  django packages
-from django.shortcuts import render
+from functools import partial
+from django.shortcuts import redirect, render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status, permissions
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate, login, logout
 
 # project packages
-from projectmanager.models import Project
+from projectmanager.models import Project, User
 from .serializers import ProjectSerializer
 
 # for testing purpose
@@ -19,29 +22,38 @@ from .serializers import ProjectSerializer
 #     else:
 #         return '{0}{1}/{2}'.format('http://', host, 'url')
 
-@api_view(['GET'])
-def getData(request):
-    person = Project.objects.all()
-    serializer = ProjectSerializer(person, many=True)
-    return Response(request, serializer.data)
+# @api_view(['GET'])
+# def getData(request):
+#     person = Project.objects.all()
+#     serializer = ProjectSerializer(person, many=True)
+#     return Response(request, serializer.data)
 
-@api_view(['POST'])
-def addData(request):
-    serializer = ProjectSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(request, serializer.data)
+# @api_view(['POST'])
+# def addData(request):
+#     serializer = ProjectSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(request, serializer.data)
 
-def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        # check if the user is exist
-        try:
-            user = User.objects.get(username=username)
-        except:
-            messages.error(request, 'User does not exit')
-    return Response(username)
+# def loginPage(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         # check if the user is exist
+#         try:
+#             user = User.objects.get(username=username)
+#         except:
+#             messages.error(request, 'User does not exit')
+            
+#         user = authenticate(request, username=username, password=password)
+        
+#         if user is not None:
+#             login(request,user) #add database of user
+#             return redirect('home') # return the user to homepage for now just placeholder
+#         else:
+#             messages.error(request, 'Username or Password does not exit')    
+    
+#     return Response(username) #placeholder for now
 
 
 # """
@@ -51,26 +63,63 @@ def loginPage(request):
 #     description = models.TextField(blank=True, null=True)
 # """
 
-# class ProjectViewSet(ModelViewSet):
-#     queryset = Project.objects.all()
-#     serializer_class = ProjectSerializer
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
-#     @action(detail=False, methods=['get'])
-#     def projects(self, request):
-#         projects = Project.objects.all()
-#         serializer = ProjectSerializer(projects, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
+    @action(detail=False, methods=['get'])
+    def projects(self, request):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-#     @action(detail=True, methods=['post'])
-#     def create(self, request, *args, **kwargs):
-#         serializer = ProjectSerializer(data=request.data)
-#         if serializer.is_valid():
-#             project = Project()
-#             project.user = User.objects.get(pk=request.user.id)
-#             project.title = (serializer.validated_data['title'])
-#             project.tools = (serializer.validated_data['tools'])
-#             project.description = (serializer.validated_data['description'])
-#             project.save()
-#             return Response({'status':'new project created'})
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @action(detail=True, methods=['post'])
+    def create(self, request, *args, **kwargs):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            project = Project()
+            #project.user = User.objects.get(pk=request.user.id)
+            project.user = User.objects.get(pk=1) # for testing
+            project.title = (serializer.validated_data['title'])
+            project.tools = (serializer.validated_data['tools'])
+            project.description = (serializer.validated_data['description'])
+            project.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['patch'])
+    def update(self, request, *args, **kwargs):
+        serializer = ProjectSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            if request.user.id == serializer.validated_data['user']:
+                t = (serializer.validated_data['title'])
+                project = get_object_or_404(Project, title=t)
+                project.images = (serializer.validated_data['images'])
+                project.save()
+
+                return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    @action(detail=True, methods=['post'])
+    def loginPage(self, request):
+        serializer = ProjectSerializer(data=request.data, partial=True)
+        user = User()
+        user.username = request.POST.get('username')
+        user.password = request.POST.get('password')
+        # check if the user is exist
+        try:
+            user = User.objects.get(username=user.username)
+        except:
+            messages.error(request, 'User does not exit')
+            
+        userAu = authenticate(request, username=user.username, password=user.password)
+        
+        if userAu is not None:
+            login(request,user) #add database of user
+            return redirect('home') # return the user to homepage for now just placeholder
+        else:
+            messages.error(request, 'Username or Password does not exit')    
+        context = {}
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED) #placeholder for now    
