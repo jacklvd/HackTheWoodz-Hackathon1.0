7696d1b52a33002026f0d67169cdc8e8adc61dbf
geuresti <gioeuresti123@gmail.com>
2022-06-05 00:43:16 -0500

    added get endpoint for image, editted other endpoints

--- backend/projectmanager/urls.py
+++ backend/projectmanager/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('projects/', ProjectViewSet.as_view({'get':'projects'})),
     path('create/', ProjectViewSet.as_view({'post':'create'})),
     path('update/', ProjectViewSet.as_view({'post':'update'})),
+    path('get_image/', ProjectViewSet.as_view({'get':'get_image'})),
 ]
 
 # name, job title, all images, project title, project desc
--- backend/projectmanager/views.py
+++ backend/projectmanager/views.py
@@ -9,6 +9,7 @@ from .serializers import ProjectSerializer
 from django.contrib.auth.models import User
 
 from django.shortcuts import get_object_or_404
+import json
 
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
@@ -17,6 +18,7 @@ class ProjectViewSet(ModelViewSet):
     @action(detail=False, methods=['get'])
     def projects(self, request):
         projects = Project.objects.all()
+
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -37,8 +39,9 @@ class ProjectViewSet(ModelViewSet):
 
             tools_data = serializer.validated_data['tools']
             description_data = serializer.validated_data['description']
+            image_data = serializer.validated_data['images']
 
-            if title_data == None or tools_data == None or description_data == None:
+            if title_data == None or tools_data == None or description_data == None or image_data == None:
                 return Response({"error":"required fields missing"})
 
             project.title = title_data
@@ -53,14 +56,34 @@ class ProjectViewSet(ModelViewSet):
     def update(self, request, *args, **kwargs):
         serializer = ProjectSerializer(data=request.data, partial=True)
         if serializer.is_valid():
-                t = (serializer.validated_data['title'])
-                project = get_object_or_404(Project, title=t)
+            # change grab by ID
+            title_data = (serializer.validated_data['title'])
+            project = get_object_or_404(Project, title=title_data)
 
         # UNCOMMENT THIS ONCE DONE TESTING WITH POSTMAN
-                if project.user: # == request.user:
-                    project.images = (serializer.validated_data['images'])
-                    project.save()
+            if project.user: # == request.user:
+            # change grab by ID
+
+                # update anything that's changed (not None)
+                tools_data = serializer.validated_data['tools']
+                description_data = serializer.validated_data['description']
+                image_data = serializer.validated_data['images']
+
+                project.images = (serializer.validated_data['images'])
+                project.save()
 
                 return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['get'])
+    def get_image(self, request, *args, **kwargs):
+        serializer = ProjectSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            title_data = (serializer.validated_data['title'])
+            project = get_object_or_404(Project, title=title_data)
+            img = "/media/" + str(project.images)
+
+            return Response({"images":img})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
